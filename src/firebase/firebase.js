69df6dfea6bd864d66f36c8b83bb5ef09ca2fefa
Bruno De Coene <brunodecoene@Brunos-MacBook-Pro-2.local>
2018-11-21 16:04:30 +0100

    html + link scss & js files

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ const firebaseInstance = require('firebase');
 // Initialize Firebase
 // TODO: Replace with your project's config
 const config = {
-  apiKey: '<key>',
-  authDomain: '<projectId>.firebaseapp.com',
-  databaseURL: 'https://<projectId>.firebaseio.com',
-  projectId: '<projectId>',
-  storageBucket: '<projectId>.appspot.com',
-  messagingSenderId: '52666332181',
+  apiKey: "AIzaSyDYqao9LCkF6oXszrykeOV4oxtoTuUvuwM",
+  authDomain: "op-kot-in-gent.firebaseapp.com",
+  databaseURL: "https://op-kot-in-gent.firebaseio.com",
+  projectId: "op-kot-in-gent",
+  storageBucket: "op-kot-in-gent.appspot.com",
+  messagingSenderId: "333922770361"
 };
 
 let instance = null;
